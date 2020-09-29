51f367d28df3d0da9645950da7e1627b98ed3058
dikirahman <dikirahman50@gmail.com>
2020-09-29 14:01:22 +0700

    updated svg icon

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ const biodata = document.getElementById('biodata');
 const programming = document.getElementById('programming');
 
 programming.addEventListener('click', function() {
-  biodata.style.backgroundImage = "url('../img/rocket/rocket-red.svg')";
+  biodata.style.backgroundImage = "url('img/rocket/rocket-red.svg')";
 });
