a40b83f4bf4a74059f362086730585ee6c9dca40
dikirahman <dikirahman50@gmail.com>
2020-09-29 11:02:36 +0700

    add animation background editing

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const biodata = document.getElementById('biodata');
+const programming = document.getElementById('programming');
+
+programming.addEventListener('click', function() {
+  biodata.style.backgroundImage = "url('../img/rocket/rocket-red.svg')";
+});
