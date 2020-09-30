f90d68a5b811f9dafb92f62dc40a1be2a56ce06e
dikirahman <dikirahman50@gmail.com>
2020-09-30 10:19:12 +0700

    add image

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 const biodata = document.getElementById('biodata');
 const programming = document.getElementById('programming');
+const design = document.getElementById('design');
+const analysis = document.getElementById('analysis');
+const dataScience = document.getElementById('data-science');
 
 programming.addEventListener('click', function() {
-  biodata.style.backgroundImage = "url('img/rocket/rocket-red.svg')";
+    biodata.style.backgroundImage = "url('img/rocket/rocket-purple.svg')";
 });
+
+design.addEventListener('click', 
+  function() {
+    biodata.style.backgroundImage = "url('img/rocket/rocket-green.svg')";
+});
+
+analysis.addEventListener('click',
+  function() {
+    biodata.style.backgroundImage = "url('img/rocket/rocket-yellow.svg')";
+});
+
+dataScience.addEventListener('click',
+  function() {
+    biodata.style.backgroundImage = "url('img/rocket/rocket-red.svg')";
+});
+
