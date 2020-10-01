43ad08bee3b6ee62dbca9ad7c9604b44337a2480
dikirahman <dikirahman50@gmail.com>
2020-10-01 15:57:36 +0700

    updated js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const programming = document.getElementById('programming');
 const design = document.getElementById('design');
 const analysis = document.getElementById('analysis');
 const dataScience = document.getElementById('data-science');
+const launchSkill = document.getElementById('launch-skill');
 
 programming.addEventListener('click', function() {
     biodata.style.backgroundImage = "url('img/rocket/rocket-purple.svg')";
@@ -11,6 +12,7 @@ programming.addEventListener('click', function() {
 design.addEventListener('click', 
   function() {
     biodata.style.backgroundImage = "url('img/rocket/rocket-green.svg')";
+    launchSkill.style.display = 'block';
 });
 
 analysis.addEventListener('click',
