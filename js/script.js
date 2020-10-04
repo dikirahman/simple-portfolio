46ab921acf3e12f36fa2e0fb8f7e5e836a9bb7a8
dikirahman <dikirahman50@gmail.com>
2020-10-04 10:28:41 +0700

    updated card

--- js/script.js
+++ js/script.js
@@ -23,11 +23,56 @@ dataScience.addEventListener('click',
     biodata.style.backgroundImage = "url('img/rocket/rocket-red.svg')";
 });
 */
-
+/*
 const ubahBackground = (skill, gambar) => {
+  // ganti background
   biodata.style.backgroundImage = `url(img/rocket/${gambar})`;
+  // tampilkan detail skill
   launchSkill.style.display = 'block';
+  // ganti judul di header detail skill
   headerSkill.innerText = skill;
   
+  // jika parameter skill = programming
+  if(skill == 'Programming') {
+
+  }
   
+} */
+
+const skill = document.getElementById('programming');
+const programming = document.getElementById('skill-programming');
+const design = document.getElementById('skill-design');
+
+/*
+skill.addEventListener('click', function() {
+  this.classList.toggle("active");
+  if(launchSkill.style.display === "block"){
+    launchSkill.style.display = 'none';
+  } else {
+    launchSkill.style.display = 'block';
+  }
+}); */
+
+function tampilkanSkill(id, img) {
+  headerSkill.innerText = id;
+  // ganti background
+  biodata.style.backgroundImage = `url(img/rocket/${img})`;
+
+  if(id == 'Programming') {
+    if (launchSkill.style.display === "block") {
+      launchSkill.style.display = 'none';
+    } else {
+      launchSkill.style.display = 'block';
+      programming.style.display = 'block';
+      design.style.display = 'none';
+    }
+  } else if(id == 'Design') {
+    if (launchSkill.style.display === "block") {
+      launchSkill.style.display = 'none';
+    } else {
+      launchSkill.style.display = 'block';
+      design.style.display = 'block';
+      programming.style.display = 'none';
+    }
+  }
 }
